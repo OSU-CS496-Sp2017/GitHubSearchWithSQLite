0408409ecf76230691f0a42ac5f6e8cad764060c
Rob Hess <robwhess@gmail.com>
2017-06-01 10:29:02 -0700

    Turn saved search results activity into a search result click listener.

--- app/src/main/java/com/example/android/githubsearchwithsqlite/SavedSearchResultsActivity.java
+++ app/src/main/java/com/example/android/githubsearchwithsqlite/SavedSearchResultsActivity.java
@@ -1,13 +1,15 @@
 package com.example.android.githubsearchwithsqlite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.example.android.githubsearchwithsqlite.R;
+import com.example.android.githubsearchwithsqlite.utils.GitHubUtils;
 
-public class SavedSearchResultsActivity extends AppCompatActivity {
+public class SavedSearchResultsActivity extends AppCompatActivity implements GitHubSearchAdapter.OnSearchResultClickListener {
 
     private RecyclerView mSavedSearchResultsRV;
 
@@ -19,5 +21,12 @@ public class SavedSearchResultsActivity extends AppCompatActivity {
         mSavedSearchResultsRV = (RecyclerView)findViewById(R.id.rv_saved_search_results);
         mSavedSearchResultsRV.setLayoutManager(new LinearLayoutManager(this));
         mSavedSearchResultsRV.setHasFixedSize(true);
+    }
+
+    @Override
+    public void onSearchResultClick(GitHubUtils.SearchResult searchResult) {
+        Intent intent = new Intent(this, SearchResultDetailActivity.class);
+        intent.putExtra(GitHubUtils.SearchResult.EXTRA_SEARCH_RESULT, searchResult);
+        startActivity(intent);
     }
 }
