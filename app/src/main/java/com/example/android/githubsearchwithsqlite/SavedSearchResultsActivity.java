1024725a6cd1509096446da710dfb5311a657f8f
Rob Hess <robwhess@gmail.com>
2017-06-01 10:09:24 -0700

    Add RecyclerView to saved search results activity.

--- app/src/main/java/com/example/android/githubsearchwithsqlite/SavedSearchResultsActivity.java
+++ app/src/main/java/com/example/android/githubsearchwithsqlite/SavedSearchResultsActivity.java
@@ -2,14 +2,22 @@ package com.example.android.githubsearchwithsqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.android.githubsearchwithsqlite.R;
 
 public class SavedSearchResultsActivity extends AppCompatActivity {
 
+    private RecyclerView mSavedSearchResultsRV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_search_results);
+
+        mSavedSearchResultsRV = (RecyclerView)findViewById(R.id.rv_saved_search_results);
+        mSavedSearchResultsRV.setLayoutManager(new LinearLayoutManager(this));
+        mSavedSearchResultsRV.setHasFixedSize(true);
     }
 }
